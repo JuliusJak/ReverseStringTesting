05f7702639257948fbdf3ddd3322f9c4a31cfbc5
JuliusJak <julius.bis03@gmail.com>
2022-11-30 08:55:46 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        String phrase = "Hello World";
+
+
+        for (char phraseArray : phrase.toCharArray()) {
+            System.out.println(phraseArray);
+        }
+        char[] phraseArray = phrase.toCharArray();
+
+        for (int i = phrase.length()-1; i >= 0 ; i--){
+            char reverseArray = phraseArray[i];
+            System.out.println(reverseArray);
+        }
+        for (int i = phrase.length()-1; i >= 0 ; i--){
+            System.out.print(phraseArray[i]);
+        }
+    }
+}
